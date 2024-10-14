d54391704353db5d4dd5534f9a0b670f19a9f231
sagarhasan273 <sagarhasan273@gmail.com>
2024-10-15 03:44:28 +0600

    project euler . net manually

--- src/pages/ProjectEuler/ProjectEulerHome.js
+++ src/pages/ProjectEuler/ProjectEulerHome.js
@@ -1,12 +1,26 @@
 import { Stack } from '@mui/material';
 import React from 'react';
-import projectEuler from '../../assets/images/projecteuler.png';
 
 function ProjectEulerHome() {
   return (
     <Stack alignItems="center">
-      <Stack sx={{ width: '500px', height: '50vh' }} justifyContent="center">
-        <img alt="Project Euler" src={projectEuler} />
+      <Stack direction="row" sx={{ width: '650px', height: '50vh', position: 'relative' }} alignItems="center">
+        <span style={{ color: '#6B4E3D', fontSize: '80px', fontWeight: 700, fontFamily: 'sans-serif' }}>
+          Project Euler
+        </span>
+        <span
+          style={{
+            position: 'relative',
+            top: 35,
+            left: -28,
+            color: '#BABABA',
+            fontSize: '60px',
+            fontWeight: 500,
+            fontFamily: 'system-ui',
+          }}
+        >
+          .net
+        </span>
       </Stack>
     </Stack>
   );
