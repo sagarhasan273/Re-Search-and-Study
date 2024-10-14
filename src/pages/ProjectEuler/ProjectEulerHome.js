0110693aa38938e1ef61c6457d76aad86bd27859
sagarhasan273 <sagarhasan273@gmail.com>
2024-10-15 01:30:45 +0600

    home page for project euler

---
+++ src/pages/ProjectEuler/ProjectEulerHome.js
@@ -0,0 +1,15 @@
+import { Stack } from '@mui/material';
+import React from 'react';
+import projectEuler from '../../assets/images/projecteuler.png';
+
+function ProjectEulerHome() {
+  return (
+    <Stack alignItems="center">
+      <Stack sx={{ width: '700px', height: '50vh' }} justifyContent="center">
+        <img alt="Project Euler" src={projectEuler} />
+      </Stack>
+    </Stack>
+  );
+}
+
+export default ProjectEulerHome;
--- src/pages/ProjectEuler/index.js
+++ src/pages/ProjectEuler/index.js
@@ -1,13 +1,14 @@
 import { Box } from '@mui/material';
 import React from 'react';
 import { useGlobalContext } from '../../context/GlobalContext';
+import ProjectEulerHome from './ProjectEulerHome';
 
 function ProjectEuler() {
   const { state } = useGlobalContext();
   const page = state?.filter((item) => item?.isSelected)?.[0];
   return (
     <Box className="stylishScrollbar" sx={{ overflowY: 'scroll', height: '100%', pb: '40px' }}>
-      {page?.element}
+      {page?.element || <ProjectEulerHome />}
     </Box>
   );
 }
