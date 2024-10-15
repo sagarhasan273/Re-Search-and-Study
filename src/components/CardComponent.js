37a13b8e82db1ef48982918607e61b645c48a322
sagarhasan273 <sagarhasan273@gmail.com>
2024-10-16 04:51:43 +0600

    home page items added

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -2,7 +2,7 @@ import { Stack } from '@mui/material';
 import React from 'react';
 import '../assets/css/CardComponent.css'; // Assuming you're importing a separate CSS file
 
-function CardComponent({ onClick, heading, para, buttonText = 'Go' }) {
+function CardComponent({ onClick, heading, para, disabled = false, buttonText = 'Go' }) {
   return (
     <div className="card-container">
       <Stack gap={2}>
@@ -10,7 +10,7 @@ function CardComponent({ onClick, heading, para, buttonText = 'Go' }) {
         <p className="card-paragraph">{para}</p>
       </Stack>
 
-      <button type="button" className="card-button" onClick={onClick}>
+      <button type="button" className="card-button" onClick={onClick} disabled={disabled}>
         <p>{buttonText}</p>
         <svg className="card-button-icon" strokeWidth="1.5" viewBox="0 0 24 24">
           <path
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -9,6 +9,10 @@ function Home() {
   const navigate = useNavigate();
 
   const handleClick = (type) => {
+    if (type === 'github') {
+      window.open('https://github.com/sagarhasan273/', '_blank');
+      return;
+    }
     if (type === 'leetcode') {
       window.open('https://leetcode.com/u/sagarhasan273/', '_blank');
       return;
@@ -32,14 +36,19 @@ function Home() {
       sx={{ m: 'auto', overflowY: 'scroll', maxWidth: '700px', height: '100%', flexWrap: 'wrap' }}
     >
       <CardComponent
+        onClick={() => handleClick('leetcode')}
+        heading="LeetCode"
+        para="Go to my LeetCode profile! I'm focused on improving my coding skills and preparing for technical interviews through a variety of challenges."
+      />
+      <CardComponent
         onClick={() => handleClick('project_euler')}
         heading="Project Euler"
         para="Challenge your problem-solving skills with Project Euler. Tackle math-based coding puzzles and enhance your logic with each solution."
       />
       <CardComponent
-        onClick={() => handleClick('leetcode')}
-        heading="LeetCode"
-        para="Go to my LeetCode profile! I'm focused on improving my coding skills and preparing for technical interviews through a variety of challenges."
+        onClick={() => handleClick('github')}
+        heading="GitHub"
+        para="Harness the power of collaborative coding with my GitHub project, where developers come together to innovate and solve real-world problems."
       />
       <CardComponent
         onClick={() => handleClick('javascript')}
@@ -50,16 +59,19 @@ function Home() {
         onClick={() => handleClick('react')}
         heading="React"
         para="Learn React by building interactive, real-world applications. Master component-based development with step-by-step tutorials."
+        disabled
       />
       <CardComponent
         onClick={() => handleClick('node')}
         heading="Node"
         para="Master backend development with Node.js. Build fast, scalable server-side applications through hands-on coding and real-world projects."
+        disabled
       />
       <CardComponent
         onClick={() => handleClick('dom')}
         heading="DOM"
         para="Explore the Document Object Model (DOM) to manipulate web pages dynamically. Learn how to interact with HTML and CSS using JavaScript for seamless user experiences."
+        disabled
       />
     </Stack>
   );
